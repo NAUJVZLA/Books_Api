d2dea97a18ab4e197e4d9ec0ede7a69aef00ad5f
NAUJVZLA <ju4nc4r1d4d@gmail.com>
2024-07-18 11:37:38 -0500

    books subiendo a git

---
+++ src/controllers/controller.users.ts
@@ -0,0 +1,31 @@
+import {BodyResquestLogin, BodyResponseLogin} from "../models/auth.model"
+
+export class UserController {
+    public domain: string;
+    constructor(domain: string) {
+        this.domain = domain;
+    }
+    async login(email:HTMLInputElement, password:HTMLInputElement): Promise<BodyResponseLogin>{
+        const userData:BodyResquestLogin={
+            email: email.value,
+            password: password.value
+        }
+        const headers: Record<string, string> = {
+            'accept':'/',
+            'Content-Type': 'application/json'
+        }
+        const requestOptions: RequestInit = {
+            method: 'POST', //metodo de Login
+            headers: headers,
+            body: JSON.stringify(userData),
+        }
+        const response:Response =  await fetch(`${this.domain}/api/vi/auth/login`, requestOptions);
+    
+        if (!response.ok) {
+            console.log(`respondebody error:  ${(await response.json()).message}`);
+            throw new Error(`error: ${response.status}:${response.statusText}`)
+    }
+
+    const responseBodyLogin: BodyResponseLogin = await response.json();
+    return responseBodyLogin;
+}}
---
+++ src/login.ts
@@ -0,0 +1,24 @@
+import {UserController} from "./controllers/controller.users.js";
+    const URL_USERS : string = "http://190.147.64.47:5155"
+
+    const form = document.querySelector('form') as HTMLFormElement;
+    const email = document.getElementById('email') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+
+
+    form.addEventListener("submit",async (e:Event)=>{
+        e.preventDefault();
+        const crudusers = new UserController(URL_USERS);
+        const respuesta = await crudusers.login(email,password);
+
+        const token : string | null = respuesta.data.token;
+
+        if (token){
+            localStorage.setItem('token',token);
+            window.location.href="books.html";
+        }else{
+            console.log('error al ingresar ');
+        }
+        form.reset();
+
+    })
