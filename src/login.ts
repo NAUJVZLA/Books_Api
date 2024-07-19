13cdf0a3c7721885756fab86563e8fb1695861df
NAUJVZLA <ju4nc4r1d4d@gmail.com>
2024-07-18 21:03:26 -0500

    errores

--- src/login.ts
+++ src/login.ts
@@ -1,12 +1,12 @@
 import {UserController} from "./controllers/controller.users.js";
-    const URL_USERS : string = "http://190.147.64.47:5155/api/v1"
+    const URL_USERS : string = "http://190.147.64.47:5155"
 
     const form = document.querySelector('form') as HTMLFormElement;
     const email = document.getElementById('email') as HTMLInputElement;
     const password = document.getElementById('password') as HTMLInputElement;
 
 
-    form.addEventListener("submit",async (e:Event)=>{
+    form.addEventListener("submit",async (e:Event) =>{
         e.preventDefault();
         const crudusers = new UserController(URL_USERS);
         const respuesta = await crudusers.login(email,password);
